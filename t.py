9ddf89bf9a61c71256ba7137896bfbf5f8577583
Taxod <s885537@gmail.com>
2018-05-18 11:53:12 +0800

    -

--- t.py
+++ t.py
@@ -1,19 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-def find_page
-
-
-
-def find_ip_id(url):
+def find_PageHtmlText(url):
     resp = requests.get(url)
     soup = BeautifulSoup(resp.text, 'html.parser')
-    x = soup.find_all('div', 'title')
-    for i in x:
-        if i.find_all('a'):
-            i = i.find('a')
+    return soup
+
+
+def find_ip_id(Html_soup):
+    article_title_href = Html_soup.find_all('div', 'title')
+    for i in article_title_href:
+        A_tag =  i.find('a')
+        if A_tag != None:
             single_href = i.get('href')#/bbs/prozac/M.1526572117.A.C0E.html
             url_string = "https://www.ptt.cc"+single_href
+
+
+            
             re = requests.get(url_string)
             tmp = BeautifulSoup(re.text,'html.parser')
             tmp_ip = tmp.find('span','f2')
@@ -28,7 +31,7 @@ def find_ip_id(url):
 
 
 url = 'https://www.ptt.cc/bbs/prozac/index650.html'
-find_ip_id(url)
+find_ip_id(find_PageHtmlText(url))
 
 def Get_LastPage_Url(html_text):
     x = html_text.find_all('a','btn wide',limit=2)
